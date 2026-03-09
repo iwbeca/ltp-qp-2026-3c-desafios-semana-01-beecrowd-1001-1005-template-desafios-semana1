4cf18363515de4ef1e0ed5960c3e5378c1295f9c
iwbeca <rebeca.alphonsus@gmail.com>
2026-03-09 20:18:37 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -12,3 +12,9 @@ você receberá "Presentation Error".
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
 
 # Escreva sua solução abaixo
+A = int(input())
+B = int(input())
+
+X = A + B
+
+print("X =", X)
